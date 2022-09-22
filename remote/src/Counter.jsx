398d0f27105148f3bc67d30b9c3dedac63d6adfa
Dylan Apera <dylan.n.apera@hotmail.com>
2022-09-22 15:52:55 +1200

    added banners for testing

--- remote/src/Counter.jsx
+++ remote/src/Counter.jsx
@@ -4,9 +4,12 @@ export default () => {
     const [count, setCount] = createSignal(0);
 
     return(
+    <div class="bg-blue-900 text-white">
+
         <div>
             <div>count = {count()}</div>
             <button onClick={() => setCount(count() + 1)}>Add</button>
+        </div>
         </div>
     )
 }
